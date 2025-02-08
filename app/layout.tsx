2d95103df77f3eceaac6be8f16fb8046d801b59e
Abhishek Singh <as9284608@gmail.com>
2025-02-08 21:42:16 +0530

    adding required assets and setting up tailwind colors

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
-import Navigation from "./components/navigation";
+import Link from "next/link";
+import Navigation from "./_components/Navigation";
 import "./globals.css";
+import Image from "next/image";
 
 export const metadata = {
   title: "Tranquil Peaks",
@@ -13,6 +15,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <Link href="/">
+          <Image src="/logo-light.png" alt="logo" width={50} height={50} />
+        </Link>
         <Navigation />
         {children}
       </body>
